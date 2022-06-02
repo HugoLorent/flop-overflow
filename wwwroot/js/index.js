dba98ae1d91ce507642dfb8c75adda69d154d14c
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-06-02 11:01:03 +0200

    add index.js and modify index.html
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

---
+++ wwwroot/js/index.js
@@ -0,0 +1,46 @@
+﻿const postsUri = 'api/Post';
+let posts = [];
+let comments = [];
+
+function getPosts() {
+    fetch(postsUri)
+        .then((response) => response.json())
+        .then((posts) => displayPosts(posts))
+        .catch((error) => console.error('Unable to get posts : ', error));
+}
+
+function displayPosts(posts) {
+    const postsContainer = document.getElementById('posts-container');
+    posts.forEach((post) => {
+        const colDiv = document.createElement('div');
+        postsContainer.appendChild(colDiv).classList.add('col');
+
+        const cardDiv = document.createElement('div');
+        colDiv.appendChild(cardDiv).classList.add('card', 'shadow-sm');
+
+        const cardBodyDiv = document.createElement('div');
+        cardDiv.appendChild(cardBodyDiv).classList.add('card-body');
+
+        const pElement = document.createElement('p');
+        cardBodyDiv.appendChild(pElement).classList.add('card-text');
+
+        const title = document.createTextNode(post.title);
+        pElement.appendChild(title);
+
+        const contentDiv = document.createElement('div');
+        cardBodyDiv.appendChild(contentDiv).classList.add('d-flex', 'justify-content-between', 'align-items-center');
+
+        const btnDiv = document.createElement('div');
+        contentDiv.appendChild(btnDiv).classList.add('btn-group');
+
+        const btn = document.createElement('button');
+        const btnText = document.createTextNode('Check the topic');
+        btnDiv.appendChild(btn).classList.add('btn', 'btn-sm', 'btn-outline-secondary');
+        btn.appendChild(btnText);
+
+        const likesElement = document.createElement('small');
+        const likesValue = document.createTextNode(post.likes.toString());
+        contentDiv.appendChild(likesElement).classList.add('text-muted');
+        likesElement.appendChild(likesValue);
+    });
+}
