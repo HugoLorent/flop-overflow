09ef6b005514ac5d7242edfddefbcb67242428cc
LeperlierO <leperlierolivier3@gmail.com>
2022-06-02 15:21:24 +0200

    modification details

---
+++ wwwroot/js/details.js
@@ -0,0 +1,15 @@
+﻿const uri = 'https://localhost:44348/api/Post';
+
+window.onload = checkParam;
+
+function checkParam() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const id = urlParams.get('id');
+
+    fetch(uri + "/" + id)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Unable to get items.', error));
+
+}
