702fe6cfae8edea061b51ada425abd5c130b240c
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-06-05 13:39:25 +0200

    add register page
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

--- wwwroot/js/register.js
+++ wwwroot/js/register.js
@@ -1,43 +1,21 @@
-﻿const uri = 'api/Login'
+﻿const postUserUri = '/api/User';
 
-window.onload = checkToken;
+function register() {
 
-function goToRegisterPage() {
-    location.href = '/register.html'
-}
-
-function checkToken() {
-    if (check_cookie_name("token") != null) {
-        window.location.href = "index.html";
-    }
-}
-
-function check_cookie_name(name) {
-    var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-    if (match) {
-        return(match[2]);
-    }
-    else {
-        return null;
-    }
-}
-
-function signin() {
-
-    var success = true;
+    let success = true;
     const login = {
         Login: document.getElementById('login').value.trim(),
         Pwd: document.getElementById('pwd').value.trim()
     };
 
-    fetch(uri, {
+    fetch(postUserUri, {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(login)
-    })
+        })
         .then(response => {
             if (response.status === 404) {
                 alert("Try again...");
@@ -47,9 +25,8 @@ function signin() {
         })
         .then(data => {
             if (success) {
-                document.cookie = "token=" + data;
                 window.location.href = "index.html";
             }
         })
-        .catch(error => console.error('Unable to login.', error));
+        .catch(error => console.error('Unable to register.', error));
 }
